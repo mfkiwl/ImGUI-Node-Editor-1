c99e66f0663c1f1b6a17a4b98bd2033d9f98f47f
DarkImage <luc-af@live.it>
2018-07-30 17:51:28 +0200

    Aggiunto 3rd party dependencies: - GLFW - GLAD Ultimato lo script di generazione genie.lua

--- genie.lua
+++ genie.lua
@@ -1,30 +1,45 @@
 PROJ_DIR = os.getcwd()
 
 solution "Node-Editor"
-	location (path.join(PROJ_DIR,"build/"))
+location(path.join(PROJ_DIR,"build/"))
 
-	configurations {
-		"debug",
-		"release",
-	}
-	platforms {
-		"x64",
-	}
+configurations {
+	"debug",
+	"release",
+}
+platforms {
+	"x64",
+}
 
-	language "C++"
+language "C++"
 
-	project "node-editor"
-		configurations "debug"
-			targetdir (path.join(PROJ_DIR,"build/debug"))
-		configurations "release"
-			targetdir (path.join(PROJ_DIR,"build/release"))
+project "node-editor"
 
-		location "build/"
-		kind "ConsoleApp"
+	location "build/"
+	kind "ConsoleApp"
 
+	configuration "debug"
+		targetdir (path.join(PROJ_DIR,"build/debug"))
 		debugdir(path.join(PROJ_DIR,"build/debug"))
+	configuration "release"
+		targetdir (path.join(PROJ_DIR,"build/release"))
+		debugdir(path.join(PROJ_DIR,"build/release"))
 
-		files {
-			path.join(PROJ_DIR,"source/*.cpp")
-			path.join(PROJ_DIR,"source/*.h")
-		}
+	configuration {}
+	libdirs{
+		path.join(PROJ_DIR,"lib/"),
+	}
+	links {
+		"opengl32",
+		"glfw3",
+	}
+	includedirs{
+		path.join(PROJ_DIR,"include/"),
+	}
+
+
+	files {
+		path.join(PROJ_DIR,"source/*.cpp"),
+		path.join(PROJ_DIR,"source/*.c"),
+		path.join(PROJ_DIR,"source/*.h"),
+	}
--- source/node-editor.cpp
+++ source/node-editor.cpp
@@ -1,6 +1,9 @@
 // node-editor.cpp : Defines the entry point for the console application.
 //
 
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
+
 int main()
 {
     return 0;
